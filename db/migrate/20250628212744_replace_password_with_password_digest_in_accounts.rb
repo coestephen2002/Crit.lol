21cc0c2dcd75eece82d99fbfdce6b0b617c9fc3e
Stephen <66713914+coestephen2002@users.noreply.github.com>
2026-09-03 16:40:07 -0700

    Delete db directory

--- db/migrate/20250628212744_replace_password_with_password_digest_in_accounts.rb
+++
@@ -1,7 +0,0 @@
-class ReplacePasswordWithPasswordDigestInAccounts < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :accounts, :password, :string
-    remove_column :accounts, :salt, :string
-    add_column :accounts, :password_digest, :string, null: false
-  end
-end
